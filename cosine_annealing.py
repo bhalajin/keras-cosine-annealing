96eac16c0db42d61423b292f0fa23969fc127b07
Bhalaji <bhalaji@solitontech.com>
2019-06-12 15:32:53 +0200

    Added T_multi variable to improve anytime performance
    Signed-off-by: Bhalaji N <bhalaji10@gmail.com>

--- cosine_annealing.py
+++ cosine_annealing.py
@@ -7,10 +7,11 @@ class CosineAnnealingScheduler(Callback):
     """Cosine annealing scheduler.
     """
 
-    def __init__(self, T_max, eta_max, eta_min=0, T_cur=0, verbose=0):
+    def __init__(self, T_max, eta_max, eta_min=0, T_cur=0, T_multi=1, verbose=0):
         super(CosineAnnealingScheduler, self).__init__()
         self.T_max = T_max
         self.T_cur = T_cur
+        self.T_multi = T_multi  # to improve anytime performance
         self.eta_max = eta_max
         self.eta_min = eta_min
         self.verbose = verbose
@@ -21,6 +22,7 @@ class CosineAnnealingScheduler(Callback):
         lr = self.eta_min + (self.eta_max - self.eta_min) * (1 + math.cos(math.pi * self.T_cur / self.T_max)) / 2
         if self.T_cur == self.T_max:
             self.T_cur = 0  # restart
+            self.T_max = self.T_max * self.T_multi
         else:
             self.T_cur = self.T_cur + 1
         K.set_value(self.model.optimizer.lr, lr)
